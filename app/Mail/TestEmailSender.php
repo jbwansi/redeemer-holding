d56f9ce3d5683657db58d342cd5c57e3c2bf3fc6
Carlos Alognon <carlosalognon08@gmail.com>
2025-02-22 22:21:39 +0100

    test

--- app/Mail/TestEmailSender.php
+++ app/Mail/TestEmailSender.php
@@ -37,7 +37,7 @@ class TestEmailSender extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'view.name',
+            view: 'emails.mail-tester',
         );
     }
 
